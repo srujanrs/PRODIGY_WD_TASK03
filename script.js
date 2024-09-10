c193fc4f293f1dbc0ec1fa2f59522e7d7724e2e4
srujanrs <srujanrs30@gmail.com>
2024-09-10 20:20:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
